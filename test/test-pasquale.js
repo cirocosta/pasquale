a60cb64c688c19816cc93dffd0eca363389faf09
Ciro S. Costa <ciro.costa@usp.br>
2014-06-18 01:50:49 -0300

    adds test

--- test/test-pasquale.js
+++ test/test-pasquale.js
@@ -6,14 +6,47 @@ var Pasquale = require('../src/main.js')
 
 describe('Pasquale', function () {
 
-	beforeEach(function () {
-		pasquale = new Pasquale();
-	});
+	pasquale = new Pasquale();
+	pasquale.setLanguage('pt-br');
 
 	it('should be defined', function () {
 		assert(!!pasquale);
 	});
 
+	describe('when checking,', function () {
+		it('should not have suggestions in a correct text', function (done) {
+			var text = 'isto está ok'
 
+			pasquale.checkTextSpell('palavras corretas').then(function (results) {
+
+				results.forEach(function (result) {
+					assert(!result.suggestions);
+				});
+
+				done();
+			}, function (err) {
+				done(err);
+			});
+		});
+
+
+		it('should have suggestions in an incorrect text', function (done) {
+			var text = 'isto está ok'
+
+			pasquale.checkTextSpell('palavras correttas').then(function (results) {
+				var oneSuggestion = false;
+
+				results.forEach(function (result) {
+					if (result.suggestions)
+						oneSuggestion = true;
+				});
+
+				assert(oneSuggestion);
+				done();
+			}, function (err) {
+				done(err);
+			});
+		});
+	});
 
 });
