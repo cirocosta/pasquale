5e3307d51e04140e6b376ae24e5a0f2f41592ba6
Ciro S. Costa <ciro.costa@usp.br>
2014-06-22 12:25:48 -0300

    turns tabs to spaces

--- src/main.js
+++ src/main.js
@@ -1,92 +1,92 @@
 'use strict';
 
 var fs = require('fs')
-	,	path = require('path')
-	,	Q = require('q')
-	,	unicodeHack = require('../lib/unicode_hack')
-	,	langMap = require('./lang-mapping.json')
-	,	Spellcheck = require('spellcheck');
+  , path = require('path')
+  , Q = require('q')
+  , unicodeHack = require('../lib/unicode_hack')
+  , langMap = require('./lang-mapping.json')
+  , Spellcheck = require('spellcheck');
 
 function Pasquale () {}
 
 Pasquale.prototype.setLanguage = function(lang) {
-	var langPaths = langMap[lang]
-		,	dictPath = ''
-		, affPath = ''
-		, dicPath = '';
+  var langPaths = langMap[lang]
+    , dictPath = ''
+    , affPath = ''
+    , dicPath = '';
 
-	if (!langPaths) throw new Error('No path defined for this language');
+  if (!langPaths) throw new Error('No path defined for this language');
 
-	dictPath = path.resolve(__dirname, '../bower_components/Dictionaries');
-	affPath = path.join(dictPath, langPaths.aff);
-	dicPath = path.join(dictPath, langPaths.dic);
+  dictPath = path.resolve(__dirname, '../bower_components/Dictionaries');
+  affPath = path.join(dictPath, langPaths.aff);
+  dicPath = path.join(dictPath, langPaths.dic);
 
-	if (!(affPath.match(/\.aff$/) || dicPath.match(/\.aff$/)))
-		throw new Error('.aff and a .dic files must be specified');
+  if (!(affPath.match(/\.aff$/) || dicPath.match(/\.aff$/)))
+    throw new Error('.aff and a .dic files must be specified');
 
-	if (!(fs.existsSync(affPath) || fs.existsSync(dicPath)))
-		throw new Error('Both of the specified files must exist');
+  if (!(fs.existsSync(affPath) || fs.existsSync(dicPath)))
+    throw new Error('Both of the specified files must exist');
 
-	this.sp = new Spellcheck(affPath, dicPath);
+  this.sp = new Spellcheck(affPath, dicPath);
 };
 
 Pasquale.prototype.checkTextSpell = function (text) {
-	var lines = text.split('\n')
-		,	promises = []
-		,	dfd = Q.defer();
+  var lines = text.split('\n')
+    , promises = []
+    , dfd = Q.defer();
 
-	for (var i in lines)
-		promises.push(this.checkLineSpell(lines[i]));
+  for (var i in lines)
+    promises.push(this.checkLineSpell(lines[i]));
 
-	Q.all(promises)
-		.then(function (results) {
-			dfd.resolve(results);
-		}, function (err) {
-			dfd.reject(err);
-		});
+  Q.all(promises)
+    .then(function (results) {
+      dfd.resolve(results);
+    }, function (err) {
+      dfd.reject(err);
+    });
 
-	return dfd.promise;
+  return dfd.promise;
 };
 
 Pasquale.prototype.checkLineSpell = function (text) {
-	var regex = unicodeHack(/\p{L}+/gi)
-		,	scope = this
-		,	promises = []
-		,	dfd = Q.defer()
-		,	match;
+  var regex = unicodeHack(/\p{L}+/gi)
+    , scope = this
+    , promises = []
+    , dfd = Q.defer()
+    , match;
 
-	while (match = regex.exec(text), match)
-		promises.push(scope.checkWordSpell(match[0], match));
+  while (match = regex.exec(text), match)
+    promises.push(scope.checkWordSpell(match[0], match));
 
-	Q.all(promises)
-		.then(function (results) {
-			dfd.resolve(results);
-		}, function (err) {
-			dfd.reject(err);
-		});
+  Q.all(promises)
+    .then(function (results) {
+      dfd.resolve(results);
+    }, function (err) {
+      dfd.reject(err);
+    });
 
-	return dfd.promise;
+  return dfd.promise;
 };
 
 Pasquale.prototype.checkWordSpell = function (word, opts) {
-	var dfd = Q.defer();
+  var dfd = Q.defer();
 
-	this.sp.check(word, function (err, correct, suggestions) {
-		if (err) dfd.reject(err);
+  this.sp.check(word, function (err, correct, suggestions) {
+    if (err) dfd.reject(err);
 
-		if (correct) dfd.resolve({word: word, correct: true});
-		else dfd.resolve({
-			word: word,
-			correct: false,
-			position: {
-				l: 0,
-				c: 0
-			},
-			suggestions: suggestions
-		});
-	});
+    if (correct) dfd.resolve({word: word, correct: true});
+    else dfd.resolve({
+      word: word,
+      correct: false,
+      position: {
+        l: 0,
+        c: 0
+      },
+      suggestions: suggestions
+    });
+  });
 
-	return dfd.promise;
+  return dfd.promise;
 };
 
 module.exports = Pasquale;
--- test/test-pasquale.js
+++ test/test-pasquale.js
@@ -1,72 +1,72 @@
 'use strict';
 
 var Pasquale = require('../src/main.js')
-	,	assert = require('assert')
-	,	utils = require('./utils')
-	, pasquale = null;
+  , assert = require('assert')
+  , utils = require('./utils')
+  , pasquale = null;
 
 describe('Pasquale', function () {
 
-	pasquale = new Pasquale();
-	pasquale.setLanguage('pt-br');
+  pasquale = new Pasquale();
+  pasquale.setLanguage('pt-br');
 
-	it('should be defined', function () {
-		assert(!!pasquale);
-	});
+  it('should be defined', function () {
+    assert(!!pasquale);
+  });
 
-	describe('when checking,', function () {
-		it('should not have suggestions in a correct text', function (done) {
-			pasquale.checkTextSpell('palavras corretas').then(function (results) {
-				assert(!(!!utils.suggestionFound(results)));
-				done();
-			}, function (err) {
-				done(err);
-			});
-		});
+  describe('when checking,', function () {
+    it('should not have suggestions in a correct text', function (done) {
+      pasquale.checkTextSpell('palavras corretas').then(function (results) {
+        assert(!(!!utils.suggestionFound(results)));
+        done();
+      }, function (err) {
+        done(err);
+      });
+    });
 
-		it('should have suggestions in an incorrect text', function (done) {
-			var text = 'isto está ok'
+    it('should have suggestions in an incorrect text', function (done) {
+      var text = 'isto está ok'
 
-			pasquale.checkTextSpell('palavras correttas').then(function (results) {
-				assert(!!utils.suggestionFound(results));
-				done();
-			}, function (err) {
-				done(err);
-			});
-		});
+      pasquale.checkTextSpell('palavras correttas').then(function (results) {
+        assert(!!utils.suggestionFound(results));
+        done();
+      }, function (err) {
+        done(err);
+      });
+    });
 
-		describe('regarding multiline texts', function () {
-			var text = {
-				correct: 'isto é\num texto\nmulti linha',
-				wrong: 'issto é\num testo\nmulti linha'
-			};
+    describe('regarding multiline texts', function () {
+      var text = {
+        correct: 'isto é\num texto\nmulti linha',
+        wrong: 'issto é\num testo\nmulti linha'
+      };
 
-			it('should find suggestions in an incorrect text', function (done) {
-				pasquale.checkTextSpell(text.wrong).then(function (results) {
-					assert(utils.suggestionFound(results));
-					done();
-				});
-			});
+      it('should find suggestions in an incorrect text', function (done) {
+        pasquale.checkTextSpell(text.wrong).then(function (results) {
+          assert(utils.suggestionFound(results));
+          done();
+        });
+      });
 
-			it('should not find suggestions in a correct text', function (done) {
-				pasquale.checkTextSpell(text.correct).then(function (results) {
-					assert(!utils.suggestionFound(results));
-					done();
-				});
-			});
+      it('should not find suggestions in a correct text', function (done) {
+        pasquale.checkTextSpell(text.correct).then(function (results) {
+          assert(!utils.suggestionFound(results));
+          done();
+        });
+      });
 
-			// describe('regarding the position of the errros', function () {
-			// 	it('should be possible to get the line position', function (done) {
-			// 		pasquale.checkTextSpell(text.wrong).then(function (results) {
-			// 			var fstSugst = utils.suggestionFound(results);
+      // describe('regarding the position of the errros', function () {
+      //  it('should be possible to get the line position', function (done) {
+      //    pasquale.checkTextSpell(text.wrong).then(function (results) {
+      //      var fstSugst = utils.suggestionFound(results);
 
-			// 			assert(true);
-			// 			done();
-			// 		});
-			// 	});
+      //      assert(true);
+      //      done();
+      //    });
+      //  });
 
-			// });
-		});
-	});
+      // });
+    });
+  });
 
 });
